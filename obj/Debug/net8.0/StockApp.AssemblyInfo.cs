1adfddd6dde46f70a1d0e44b7788cf7eb702341c
Dan <knight.daniel.r@gmail.com>
2025-03-13 18:14:17 -0400

    last

--- obj/Debug/net8.0/StockApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StockApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5463d24f3361ec01950db16a7f4e39cfea59c95f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23458e81e147ee20a1deb87559d2f728d9c547cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
